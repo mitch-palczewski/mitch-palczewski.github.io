c41d61f8e3daf5054685b9bb1078a637aeff1937
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-06-10 14:47:13 -0400

    Publishing website

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/*Required Script*/ 
+function openMessageFrom(title, media, caption) {
+  document.getElementById("message_title").value = title;
+  document.getElementById("message_image").value = media;
+  document.getElementById("message_media").src = media
+  document.getElementById("message_caption").value = caption;
+  document.getElementById("message_popup").classList.add("active");
+}
+
+function closeMessageForm() {
+  document.getElementById("message_popup").classList.remove("active");
+}
+/*Required Script End*/
